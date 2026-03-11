5229df4bd84158c8986fae4f0d8fa65a6b036c08
multydigitalbusiness-hub <multydigitalbusiness@gmail.com>
2026-03-11 03:22:08 -0300

    Ajuste no workflow de deploy

---
+++ ai_studio_code.sh
@@ -0,0 +1,7 @@
+# 1. Baixe a alteração que eu fiz no arquivo de workflow
+# (Ou exporte o ZIP novamente e substitua os arquivos)
+
+# 2. Envie para o GitHub
+git add .
+git commit -m "Ajuste no workflow de deploy"
+git push origin main
